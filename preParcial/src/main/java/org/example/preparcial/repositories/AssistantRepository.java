f56ec34cc688f4f22511cf60222c7990f1675f28
Ricardo Lopez <00107521@uca.edu.sv>
2024-06-07 00:14:34 -0600

    Refactor

--- preParcial/src/main/java/org/example/preparcial/repositories/AssistantRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.preparcial.repositories;
-
-import org.example.preparcial.domain.entities.Assist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AssistantRepository extends JpaRepository<Assist, UUID> {
-}
--- preParcial/src/main/java/org/example/preparcial/repositories/TakesRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.preparcial.repositories;
-
-import org.example.preparcial.domain.entities.Take;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TakesRepository extends JpaRepository<Take, UUID> {
-}
